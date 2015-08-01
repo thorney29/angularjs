933076441ccf8d56c8317139390a0a61cca12090
Tresa Horney <tresa.e@gmail.com>
2015-07-31 23:40:46 -0700

    comments or practice code changes

--- src/angularjstutorial.js
+++ src/angularjstutorial.js
@@ -1,8 +1,49 @@
 /* use strict */
 
+/**
+ * @ngdoc object
+ * @name MyApp
+ * @description
+ *
+ * This is the module for MyApp. It uses ngRoute.
+**/
+
 var app = angular.module('MyApp', ['ngRoute'])
 
+/**
+ * @ngdoc controller
+ * @name MyApp.controller:MainCtrl
+ * @description
+ *
+ * This is the main controller. It creates a variable
+ * "message" on the scope and defines it with a string.
+ * <pre>
+
+ 		<div data-ng-controller="MainCtrl"></div>
+ 		
+ * </pre>
+**/
 .controller("MainCtrl", function ($scope)
 {
 	$scope.message = "I am an annotated message.";
+})
+
+/**
+ * @ngdoc directive
+ * @name MyApp.directive:maindirective
+ * @restrict A
+ * @priority 1000
+ * @description
+ *
+ * This is the main directive of the application. It
+ * is restricted to attributes and uses the 
+ * Main Controller.
+**/
+.directive("maindirective", function ()
+{
+	return {
+		restrict: 'A',
+		transclude: false,
+		controller: 'MainCtrl'
+	}
 })
