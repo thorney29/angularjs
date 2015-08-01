a8b611b085620880da31902ef0e3d1b48074c517
Tresa Horney <tresa.e@gmail.com>
2015-07-31 20:39:09 -0700

    edited stylesheet, binding.js and played around in other files

--- src/resources/js/angularjstutorial.js
+++ src/resources/js/angularjstutorial.js
@@ -3,6 +3,16 @@ var app = angular.module('MyApp', []);
 app.controller("MainCtrl", function ($scope)
 {
 	$scope.data = {
-		label: "Click for a different view."
+		label: "My Button 2"
 	};
 });
+
+var app = angular.module('MyApp', []);
+app.controller("MainCtrl", function ($scope) 
+{
+ 	$scope.data = {
+
+ 		labelInput: "Type in the box to the view you want",
+ 		labelButton: "View Alternate"
+ 	}
+})
--- src/resources/js/binding.js
+++ src/resources/js/binding.js
@@ -6,3 +6,13 @@ app.controller("MainCtrl", function ($scope)
 		label: "My Button 2"
 	};
 });
+
+var app = angular.module('MyApp', []);
+app.controller("MainCtrl", function ($scope) 
+{
+ 	$scope.data = {
+
+ 		labelInput: "Type in the box to the view you want",
+ 		labelButton: "View Alternate"
+ 	}
+})
